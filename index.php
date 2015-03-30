a60e27cbecda64534378fadb3762de97558d52c5
Olena Likhytska <olenalikhytska@gmail.com>
2015-03-30 14:31:03 +0300

    second commit

--- index.php
+++ index.php
@@ -1,87 +1,37 @@
-<?php
-ini_set('display_errors',0);
-if( isset( $_REQUEST['submit'] ))
-{
-$operator=$_REQUEST['operator'];
-if($operator=="+")
-{
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1+$add2;
-}
-if($operator=="-")
-{
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1-$add2;
-}
-if($operator=="*")
-{
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res =$add1*$add2;
-}
-if($operator=="/")
-{
-$add1 = $_REQUEST['fvalue'];
-$add2 = $_REQUEST['lvalue'];
-$res= $add1/$add2;
-}
-if($_REQUEST['fvalue']==NULL && $_REQUEST['lvalue']==NULL)
-{
-echo "<script language=javascript> alert(\"Please Enter values.\");</script>";
-}
-else if($_REQUEST['fvalue']==NULL)
-{
-echo "<script language=javascript> alert(\"Please Enter First value.\");</script>";
-}
-else if($_REQUEST['lvalue']==NULL)
-{
-echo "<script language=javascript> alert(\"Please Enter second value.\");</script>";
-}
-}
-?>
-<!doctype html>
 <html>
 <head>
-    <title>Calculator</title>
- 
+    <title>Webpage in PHP</title>
 </head>
-<h1>My first calculator</h1>
 <body>
-<form>
-<table style="border:groove white">
-            <tr>
-                <td style="background-color:pink; color:black; font-family:'Arial'">Enter First Number</td>
-                <td colspan="1">
-               
-                    <input name="fvalue" type="text" style="color:black"/></td>
-            <tr>
-                <td style="color:black; font-family:'Arial'">Select Operator</td>
-                <td>
-                    <select name="operator" style="width: 63px">
-<option>+</option>
-<option>-</option>
-<option>*</option>
-<option>/</option>
-</select></td>
-               </tr>
-            <tr>
-                <td style="background-color:pink; color:black; font-family:'Arial'">Enter First Number</td>
-                <td class="auto-style5">
-                    <input name="lvalue" type="text"  style="color:black"/></td>
-               
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" name="submit" value="Submit" style="color:white;background-color:rosybrown" /></td>
-               
-            </tr>
-            <tr>
-                <td style="background-color:white;color:black; font-family:'Arial'">Result = </td>
-                <td style="color:darkblue"><?php echo $res;?></td>
-               
-            </tr>
-       </table>
- </form>
-	</body>
+ <div style="line-height:40px;">
+ <div>
+  <form action="adduser.php">
+	  Name<br><input type="text" name="name" maxlength="20" size="25" padding: 5px;></form>
+		Surname<br><input type="text" name="surname" maxlength="20" size="25"><br>
+	  Country<br><input type="text" name="country" maxlength="20" size="25"></form><br>
+	  City<br><input type="text" name="city" maxlength="20" size="25"></form><br>
+	  Street<br><input type="text" name="street" maxlength="20" size="25"></form><br>
+	  Email<br><input type="text" name="email" maxlength="20" size="25"></form><br>
+Password<br><input type="text" name="confirm_pass" maxlength="20" size="25"><br>
+		<input type="submit" value="Registration">
+	</form><br>
+	<form enctype="multipart/form-date" action=savefile.php"method="post">
+	<input type="hidden name=MAX_FILE_SIZE" value="1000"><br>
+	Send this file: <input name="userfile" type="file"><br>
+	<input type="submit" Value=Send File">
+	</form>
+	</div>
+<?php
+echo'<b>File was save on server name</b>'.
+	$_FILES['userfile']['tmp_name'].'<br>';
+echo'<b>On your mashin he names:</b>'.
+	$_FILES['userfile']['name'].'<br>';
+echo'<b>File size is:</b>'.
+	$_FILES['userfile']['size'].'<br>';
+echo'<b>Mime type of file:</b>'.
+	$_FILES['userfile']['type'].'<br>';
+move_uploaded_file($_FILES['userfile']['tmp_name'],"images/".$_FILES['userfile']['name']);
+
+?>
+</body>
+<html>
